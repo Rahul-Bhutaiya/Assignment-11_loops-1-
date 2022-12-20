90a1e6d724343478ac492e7880b23f35175fc390
Rahul Bhutaiya <111126404+Rahul-Bhutaiya@users.noreply.github.com>
2022-12-20 12:19:59 +0530

    Update factorial.py

--- factorial.py
+++ factorial.py
@@ -1,6 +1,9 @@
 #Write a python script to calculate factorial of a given number
 
 fact=int(input("Enter a Number : "))
-for x in range(fact,1,-1):
-    fact=fact*(x-1)
-print('Factorial :',fact)
+if fact==0:
+    print('Factorial :',1)
+else:
+    for x in range(fact,1,-1):
+        fact=fact*(x-1)
+    print('Factorial :',fact)
